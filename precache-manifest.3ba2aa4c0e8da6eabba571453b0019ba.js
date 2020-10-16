e2eec7a5a9f58a3a3e01eb4f3d9a38cfc43f0942
Chronowork1 <jianxie6464@gmail.com>
2020-10-16 12:15:43 -0700

    Updates

---
+++ precache-manifest.3ba2aa4c0e8da6eabba571453b0019ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3101b56ccc631a0867c",
+    "url": "/React-trivia/static/css/main.ded8e2df.chunk.css"
+  },
+  {
+    "revision": "a3101b56ccc631a0867c",
+    "url": "/React-trivia/static/js/main.a3101b56.chunk.js"
+  },
+  {
+    "revision": "97316575840d069a3e43",
+    "url": "/React-trivia/static/js/1.97316575.chunk.js"
+  },
+  {
+    "revision": "2b83e5acbcc4a8f9507b",
+    "url": "/React-trivia/static/js/runtime~main.2b83e5ac.js"
+  },
+  {
+    "revision": "85628c77b87bc407c76bc59b682a0c1a",
+    "url": "/React-trivia/index.html"
+  }
+];
